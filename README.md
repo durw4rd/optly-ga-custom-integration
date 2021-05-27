c213479b5cc87cffabac1014944ffdf172dbb471
Michal Fasanek <misa.fasa@gmail.com>
2021-05-27 16:38:47 +0200

    v1

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# optly-ga-custom-integration
+## Google Universal Analytics (Debugger) Integration
+
+This analytics integration expands on the functionality of the default integration. It can attach the integration string to any GA tracker it finds on the page so it works well also i.e. if GTM is used and the tracker name is not fixed. 
+
+The integration allows you to enable troubleshooting custom events in order to identify potential reasons for discrepencies between Optimizely and Universal analytics.
+
+Custom GA integration featuring: 
+    -   200 selectable custom dimensions 
+    -   Option to provide custom tracker name
+    -   Option to specifi GA tracking ID
+    -   Tracking of the time difference between when Optimizely & GA load
+    -   Customizable polling period
+    -   Debug events & debug logs
+
+
+## Setup
+
+1.	Use config.json to import this custom analytics integration.
+
+2.	Create 4 custom events within your project using the folowing API names:
+	-	failed_to_find_ga
+    -   set_ga_event
+    -   sent_ga_event
+    -   ga_error
+
+
+4. Use the above custom events to define the troubleshooting metrics within your metrics builder for the experiment you want to troubleshoot. For each event, I recommend setting up a metric measuring total & unique conversions. For the the 'sent_ga_event' also create a metric measuring 'total value' per 'conversion'. This will track the difference between the time Optimizely bucketed a user into the experiment and sending the tracking event to GA.
+
+5. Enable this integration for the experiment you would like to troubleshoot.
+
+## Settings
+
+* **Custom Dimension**: GA Custom Dimension Slot.
+* **Custom Tracker Name**: GA tracker name if you are not using the default GA tracker name. Also fine to leave empty if you don't have preference regarding the tracker that will be used by the integration.
+* **Polling Time**: How long do you want to wait for the GA object. Default is 10s - similar the the out-of-the-box GA integration.
+* **Debug events**: Set to TRUE if you wish to fire the debug events.
+* **Debug logs**: Set to TRUE if you wish to see the additional logs printed by the integration.
---
+++ integration-code.js
@@ -0,0 +1,103 @@
+var logEnabled = extension.logEnabled;
+var debugEvents = extension.debugEvents;
+var timeoutInterval = 100;
+var maxWait = extension.pollingTime;
+var waited = 0;
+
+function trackOptlyEvent(name, tags) {
+    try {
+        if (debugEvents) {
+            tags = tags || {};
+            window.optimizely.push({
+                "type": "event",
+                "eventName": name,
+                "tags": tags
+            });
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+try {
+    (function () {
+        function integrationLog(msg) {
+            if (logEnabled) {
+                console.log(msg);
+            }
+        }
+
+        function waitForGaEventApi(method) {
+            // this function needs to be updated to work with your particular GA implementation
+            if (window.ga && window.ga.getAll) {
+                method();
+            } else {
+                if (waited < maxWait) {
+                    setTimeout(function () {
+                        waited = waited + timeoutInterval;
+                        waitForGaEventApi(method);
+                    }, timeoutInterval);
+                } else {
+                    trackOptlyEvent("failed_to_find_ga");
+                    integrationLog("Failed to find the GA object.");
+                }
+            }
+        }
+
+        var decisionString = optimizely.get('state').getDecisionString({
+            campaignId: campaignId,
+            shouldCleanString: true,
+            maxLength: 255
+        });
+
+        waitForGaEventApi(function () {
+            if (!!decisionString) {
+                integrationLog("Preparing to send the decision event to GA.");
+                // send event to GA
+                // edit the 'fieldObject' properties as needed
+                var fieldsObject = { nonInteraction: true };
+                fieldsObject[extension.customDimension] = decisionString;
+                integrationLog(fieldsObject);
+
+                // check the GA tracking ID associated with every tracker and select a tracker accordingly
+                if (!!extension.trackingID) {
+                    integrationLog("Tracking ID is set to: " + extension.trackingID);
+                    var GAtrackers = window.ga.getAll();
+                    var trackerObjects = [];
+                    for (i = 0; i < GAtrackers.length; i++) {
+                        var trackerObject = {};
+                        trackerObject['trackerName'] = window.ga.getAll()[i].get('name');
+                        trackerObject['trackingID'] = window.ga.getAll()[i].get('trackingId');
+                        trackerObjects.push(trackerObject);
+                    }
+                    var matchingTrackerObjects = trackerObjects.filter(function (trackerObject) {
+                        return trackerObject.trackingID == extension.trackingID;
+                    });
+                    if (matchingTrackerObjects.length == 0) {
+                        integrationLog("No tracker with associated to the selected GA Tracking ID found! NOT sending the GA event.");
+                        return;
+                    } else {
+                        var prefix = extension.customTrackerName || matchingTrackerObjects[0].name;
+                    }
+                } else {
+                  	// use any available tracker
+                    var prefix = extension.customTrackerName || window.ga.getAll()[0].get('name');
+                }
+                integrationLog("Name of the tracker used by the integration: " + prefix);
+
+                window.ga(prefix + '.set', extension.customDimension, decisionString);
+                trackOptlyEvent("set_ga_event");
+                window.ga(prefix + '.send', "event", "Optimizely", "Campaign decided", fieldsObject);
+                trackOptlyEvent("sent_ga_event", {
+                    value: waited
+                });
+                integrationLog("GA event sent. Time waited: " + waited);
+            }
+        });
+    })();
+} catch (error) {
+    trackOptlyEvent("ga_error", {
+        errormessage: error.message
+    });
+    integrationLog("Error encountered when trying to send the GA event: " + error.message);
+}
